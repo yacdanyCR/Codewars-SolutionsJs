a313a4a71ee5242b37c2c16fb3e143716a190b82
Yacdany <jack20-21@hotmail.com>
2023-06-22 21:20:11 -0600

    Adding

--- 7-kiu/Alternating Among Three Values.js
+++ 7-kiu/Alternating Among Three Values.js
@@ -1,3 +1,8 @@
+// Alternating Among Three Values
+// Suppose a variable x can have only three possible different values a, b and c, and you wish to assign to x the value other than its current one, and you wish your code to be independent of the values of a, b and c.
+
+// What is the most efficient way to cycle among three values? Write a function f so that it satisfies
+
 function f(x, cc) {
     const objectKeys = Object.keys(cc)
     const keyIndex = Object.values(cc).findIndex((el) => el === x)
