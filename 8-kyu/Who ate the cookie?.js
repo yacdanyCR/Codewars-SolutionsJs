af0d0f867e9826cdfc12e87b5818058d814444c4
Yacdany <jack20-21@hotmail.com>
2023-07-16 12:32:44 -0600

    Collatz conjecture length

---
+++ 8-kyu/Who ate the cookie?.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// For this problem you must create a program that says who ate the last cookie. If the input is a string then "Zach" ate the cookie. If the input is a float or an int then "Monica" ate the cookie. If the input is anything else "the dog" ate the cookie. The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+// Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+
+// Note: Make sure you return the correct message with correct spaces and punctuation.
+
+// Please leave feedback for this kata. Cheers!
+function cookie(x) {
+    const message = {
+        person: (name) => {
+            return `Who ate the last cookie? It was ${name}!`
+        },
+        dog: (name) => {
+            return `Who ate the last cookie? It was the ${name}!`
+        }
+    }
+    return typeof x === "number" ? message.person('Monica') :
+        typeof x === "string" ? message.person('Zach') :
+            message.dog('dog')
+}
